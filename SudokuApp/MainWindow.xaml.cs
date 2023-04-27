67a12682f389e587f0294fd1fa39a7bcdc5003dc
fbaldhagen <baldhagen@gmail.com>
2023-04-27 19:27:02 +0200

    adding files

---
+++ SudokuApp/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using SudokuApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SudokuApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void NewGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            NewGameButton.IsEnabled = false;
+            SolveButton.IsEnabled = false;
+            ClearButton.IsEnabled = false;
+
+            SudokuGenerator generator = new SudokuGenerator();
+
+            int[,] newPuzzle = await Task.Run(() => generator.Generate());
+
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox textBox = GetTextBoxAt(row, col);
+                    int value = newPuzzle[row, col];
+                    textBox.Text = value == 0 ? string.Empty : value.ToString();
+                }
+            }
+
+            NewGameButton.IsEnabled = true;
+            SolveButton.IsEnabled = true;
+            ClearButton.IsEnabled = true;
+        }
+
+
+
+        private void SolveButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[,] board = new int[9, 9];
+
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox textBox = GetTextBoxAt(row, col); 
+                    int.TryParse(textBox.Text, out int value);
+                    board[row, col] = value;
+                }
+            }
+
+            SudokuGenerator solver = new SudokuGenerator();
+            solver.Solve(board);
+
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox textBox = GetTextBoxAt(row, col);
+                    textBox.Text = board[row, col].ToString();
+                }
+            }
+        }
+
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    TextBox textBox = GetTextBoxAt(row, col); 
+                    textBox.Text = string.Empty;
+                }
+            }
+        }
+        private TextBox GetTextBoxAt(int row, int col)
+        {
+            int boxRow = row / 3;
+            int boxCol = col / 3;
+            int inBoxRow = row % 3;
+            int inBoxCol = col % 3;
+
+            int cellIndex = boxRow * 27 + inBoxRow * 3 + boxCol * 9 + inBoxCol;
+            string textBoxName = $"Cell{cellIndex:D2}";
+            TextBox textBox = (TextBox)this.FindName(textBoxName);
+            return textBox;
+        }
+
+
+    }
+}
---
+++ SudokuApp/Models/SudokuGenerator.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SudokuApp.Models
+{
+    public class SudokuGenerator
+    {
+        private static readonly Random random = new Random();
+
+        public int[,] Generate()
+        {
+            int[,] board = new int[9, 9];
+
+            FillValues(board);
+
+            // Remove some numbers to create the puzzle
+            RemoveNumbers(board);
+
+            return board;
+        }
+
+        private void FillValues(int[,] board)
+        {
+            FillDiagonal(board);
+
+            FillRemaining(0, 3, board);
+        }
+
+        private void FillDiagonal(int[,] board)
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                FillBox(board, i, i);
+            }
+        }
+
+        private void FillBox(int[,] board, int row, int col)
+        {
+            int[] cellNumbers = Enumerable.Range(1, 9).ToArray();
+
+            Shuffle(cellNumbers);
+
+            int count = 0;
+
+            for (int currRow = row; currRow < row + 3; currRow++)
+            {
+                for (int currCol = col; currCol < col + 3; currCol++)
+                {
+                    board[currRow, currCol] = cellNumbers[count];
+                    count++;
+                }
+            }
+        }
+
+        private bool FillRemaining(int i, int j, int[,] board)
+        {
+            if (j >= 9 && i < 9 - 1)
+            {
+                j = 0;
+                i++;
+            }
+
+            if (i >= 9 && j >= 9)
+            {
+                return true;
+            }
+
+            if (i < 3)
+            {
+                if (j < 3)
+                {
+                    j = 3;
+                }
+            }
+            else if (i < 6)
+            {
+                if (j == (i / 3) * 3)
+                {
+                    j += 3;
+                }
+            }
+            else
+            {
+                if (j == 6)
+                {
+                    j = 0;
+                    i++;
+
+                    if (i >= 9)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsSafe(i, j, num, board))
+                {
+                    board[i, j ] = num;
+                    if (FillRemaining(i, j + 1, board))
+                    {
+                        return true;
+                    }
+                    board[i, j] = 0;
+                }
+            }
+            return false;
+        }
+
+        private bool IsSafe(int i, int j, int num, int[,] board)
+        {
+            return (ValidRow(i, num, board) &&
+        ValidCol(j, num, board) &&
+        ValidBox(i - i % 3,
+        j - j % 3,
+        num, board));
+        }
+        
+        private static bool ValidRow(int row, int num, int[,] board)
+        {
+            for (int col = 0; col < 9; col++)
+            {
+                if (board[row, col] == num)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool ValidCol(int col, int num, int[,] board)
+        {
+            for (int row = 0; row < 9; row++)
+            {
+                if (board[row, col] == num)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool ValidBox(int rowStart, int colStart, int num, int[,] board)
+        {
+            for (int row = rowStart; row < rowStart + 3; row++)
+            {
+                for (int col = colStart; col < colStart + 3; col++)
+                {
+                    if (board[row, col] == num)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void Shuffle(int[] cellNumbers) 
+        {
+            int n = cellNumbers.Length;
+            while (n > 1)
+            {
+                int k = random.Next(n--);
+                int temp = cellNumbers[n];
+                cellNumbers[n] = cellNumbers[k];
+                cellNumbers[k] = temp;
+            }
+        }
+
+        public bool Solve(int[,] board)
+        {
+            int row = -1;
+            int col = -1;
+            bool isEmpty = true;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (!isEmpty)
+                {
+                    break;
+                }
+            }
+
+            if (isEmpty)
+            {
+                return true;
+            }
+
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsSafe(row, col, num, board))
+                {
+                    board[row, col] = num;
+                    if (Solve(board))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        board[row, col] = 0;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+
+        private void RemoveNumbers(int[,] board)
+        {
+            int minRemovedCells = 20; // Minimum number of cells to remove
+            int maxRemovedCells = 40; // Maximum number of cells to remove
+            int count = 0;
+
+            while (count < maxRemovedCells)
+            {
+                int row = random.Next(9);
+                int col = random.Next(9);
+
+                while (board[row, col] == 0)
+                {
+                    row = random.Next(9);
+                    col = random.Next(9);
+                }
+
+                int backup = board[row, col];
+                int[,] boardCopy = (int[,])board.Clone();
+                boardCopy[row, col] = 0;
+
+                if (HasUniqueSolution(boardCopy))
+                {
+                    board[row, col] = 0;
+                    count++;
+                }
+                else
+                {
+                    board[row, col] = backup;
+                    if (count >= minRemovedCells) // Stop removing cells if the minimum number of removed cells is reached and no more unique solutions can be found
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+
+
+        private bool HasUniqueSolution(int[,] board)
+        {
+            int solutions = 0;
+            CountSolutions(board, ref solutions);
+            return solutions == 1;
+        }
+
+        private void CountSolutions(int[,] board, ref int count)
+        {
+            int row = -1;
+            int col = -1;
+            bool isEmpty = true;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        row = i;
+                        col = j;
+                        isEmpty = false;
+                        break;
+                    }
+                }
+                if (!isEmpty)
+                {
+                    break;
+                }
+            }
+
+            if (isEmpty)
+            {
+                count++;
+                return;
+            }
+
+            for (int num = 1; num <= 9; num++)
+            {
+                if (IsSafe(row, col, num, board))
+                {
+                    board[row, col] = num;
+                    CountSolutions(board, ref count);
+                    board[row, col] = 0;
+                }
+            }
+        }
+
+    }
+}
